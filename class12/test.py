a24971abd1cc3550cf31b058bcddf6e98ad81d3a
Gaurav Ochani <GOchani@slb.com>
2020-10-03 15:58:04 +0530

    added class 1

---
+++ class12/test.py
@@ -0,0 +1,13 @@
+# uncomment the code to execute the program
+
+# print hello world prog (Ctrl + ?)
+# print("Hello World")
+
+# add 2 numbers and print
+
+# a = 10
+# b = 20
+# c = "abhilasha"
+# d = " gehi"
+# print(type(a+b))
+# print(type(c+d))
