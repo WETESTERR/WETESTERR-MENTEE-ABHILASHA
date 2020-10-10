99c7a1876780500df3f852223098a67f25de0e4d
Gaurav Ochani <GOchani@slb.com>
2020-10-10 13:37:19 +0530

    class 2 notes

--- class12/test.py
+++ class12/test.py
@@ -1,5 +1,85 @@
 # uncomment the code to execute the program
 
+
+# functions
+
+# def gaurav(arg1, arg2):
+#     print(arg1+" "+arg2)
+
+# gaurav("hello", "world")
+# gaurav("class", "python")
+
+
+# Loops
+
+# For
+# arr = ["gaurav", 5, 6.5]
+# arr = "Abhilasha"
+# for i in arr:
+#     print(i)
+
+# start, end, difference
+# for i in range(29, 21, -3):
+#     print(i)
+
+
+# print(arr)
+
+
+# While
+
+# Assignment, condition, Increment/decrement
+# i = 0
+# while i < 5:
+#     print("hello")
+#     i = i+1
+
+# i = 5
+# j = 2
+# while i > 0:
+#     while j <= 2:
+#         print("world")
+#         j = j+1
+#     print("hello")
+#     i = i-1
+
+
+# y = "abhilasha"
+# print(y)
+# print(y)
+
+
+# Variables
+
+# height_val = 10
+# width_val = 20
+
+# Multiple assignment
+# a, b, c = 10, 20, 30
+# x = y = z = 50
+# print(a, b, c)
+# print(x, y, z)
+
+
+# x = 10
+# y = "abhilasha"
+# z = x
+
+# print(x)
+# print(y)
+# print(z)
+# Indentification
+# print(id(z))
+# print(type(z))
+
+
+# Swap 2 numbers
+# x, y = 10, 20
+# print(x, y)
+# x, y = y, x
+# print(x, y)
+
+
 # print hello world prog (Ctrl + ?)
 # print("Hello World")
 
