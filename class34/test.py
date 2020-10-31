a543fbf530e8e0e5f05f657b0dbc45d7e1ff1dd9
Gaurav Ochani <gauravochani18@gmail.com>
2020-10-31 16:19:12 +0530

    added class 3 notes

---
+++ class34/test.py
@@ -0,0 +1,43 @@
+
+# Delete operator
+# a = "Gaurav"
+# # Single characters can not be deleted since strings are immutable however you can replace or delete the whole string
+# del a[0]
+# # This is allowed
+# del a
+# print(a)
+
+
+# upper
+# a = "Gaurav"
+# print(a.upper().islower())
+
+# Slicing
+# a = "Gaurav"
+# print(a[1:4])
+
+# String multiplication
+# a = "Gaurav"
+# print(4*(a))
+
+# append
+# a = "Gaurav"
+# print(a+" Ochani")
+
+
+# a = '''Hello, "How are you"'''
+# print(a[-2])
+# a = "Gaurav"
+# print(a[0])
+
+# Logical operator
+# To check return type, use "type"
+# print(type(3==4))
+
+# a = True
+# b = False
+# print(a or b)
+
+# a = 10
+# a *= 20
+# print(a)
