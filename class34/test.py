3e420898778c9fa3df6fda84f319d07d7e720b9d
Gaurav Ochani <gauravochani18@gmail.com>
2020-11-08 11:25:34 +0530

    class 4 notes added

--- class34/test.py
+++ class34/test.py
@@ -1,4 +1,59 @@
 
+# immutable
+# tuple1=(1,5,3,2)
+# tuple1[0]=10
+# print(tuple1)
+
+
+# list1=[1,5,3,2]
+# list2=[2, 9, 8, 7]
+# for x in list1:
+#     for y in list2:
+#         if x==y:
+#             print("common value:",x)
+
+
+
+
+# List
+# a = [5, "hello", True, 2,32,54,1,56,21]
+# # b = [2, "hello", True, 5]
+# # # a[0]=10
+# # # print(a)
+# # print(type(a+b))
+# a.append(1000)
+# a.remove(5)
+# print(a)
+# L1 = [1,2,3,4,5,6,7,8]
+# print(len(L1))
+
+
+
+# Days = set(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
+# print(Days)
+# print(type(Days))
+# print("looping through the set elements ... ")
+# for i in Days:
+#     print(i)
+
+# a = int(input("Enter a? "))
+# b = int(input("Enter b? "))
+# c = int(input("Enter c? "))
+# if a > b and a > c:
+#     print("a is largest")
+# if b > a and b > c:
+#     print("b is largest")
+# if c > a and c > b:
+#     print("c is largest")
+
+# a = int(input("Enter a? "))
+# b = int(input("Enter b? "))
+# if a > b:
+#     print("a is largest")
+# else:
+#     print("b is largest")
+
+
 # Delete operator
 # a = "Gaurav"
 # # Single characters can not be deleted since strings are immutable however you can replace or delete the whole string
